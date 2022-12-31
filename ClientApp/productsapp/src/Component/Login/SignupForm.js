99b87875476cf6d4df8eeff86034ecc4e5c55e30
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-31 23:21:13 +0100

    added some code for the validation in the form

--- ClientApp/productsapp/src/Component/Login/SignupForm.js
+++ ClientApp/productsapp/src/Component/Login/SignupForm.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignupForm = () => {
+  const [values, setValues] = useState({
+    fullname: "",
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
   const handleFormSubmit = (event) => {
     event.preventDefault();
   };
@@ -13,15 +26,33 @@ const SignupForm = () => {
         <form className="form-wrapper">
           <div className="name">
             <label className="label">Full Name</label>
-            <input className="input" type="text" />
+            <input
+              className="input"
+              type="text"
+              name="fullname"
+              value={values.fullname}
+              onChange={handleChange}
+            />
           </div>
           <div className="email">
             <label className="label">Email</label>
-            <input className="input" type="email" />
+            <input
+              className="input"
+              type="email"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+            />
           </div>
           <div className="password">
-            <label className="label">password</label>
-            <input className="input" type="password" />
+            <label className="label">Password</label>
+            <input
+              className="input"
+              type="password"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+            />
           </div>
           <div>
             <button className="submit" onClick={handleFormSubmit}>
